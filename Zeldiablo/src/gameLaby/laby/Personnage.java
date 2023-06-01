7c488952f82bc7c57c42aa4b9183cbf6c881d75b
nino.arcelin@gmail.com <101625357+Nin0A@users.noreply.github.com>
2023-06-01 10:43:17 +0200

    ajout des labyrinthes

--- Zeldiablo/src/gameLaby/laby/Personnage.java
+++ Zeldiablo/src/gameLaby/laby/Personnage.java
@@ -1,26 +1,2 @@
-package gameLaby.laby;
-
-public interface Personnage {
-    /**
-     * permet de savoir si le personnage est en x,y
-     *
-     * @param dx position testee
-     * @param dy position testee
-     * @return true si le personnage est bien en (dx,dy)
-     */
-    public boolean etrePresent(int dx, int dy);
-
-    // ############################################
-    // GETTER
-    // ############################################
-
-    /**
-     * @return position x du personnage
-     */
-    public int getX();
-
-    /**
-     * @return position y du personnage
-     */
-    public int getY();
+package gameLaby.laby;public class Personnage {
 }
