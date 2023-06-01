fee4366673855ed00e1ca6bcd7329c2477ef6927
nino.arcelin@gmail.com <101625357+Nin0A@users.noreply.github.com>
2023-06-01 10:44:43 +0200

    ajout d'une classe coordonnee

--- Zeldiablo/src/gameLaby/laby/Coordonnee.java
+++ Zeldiablo/src/gameLaby/laby/Coordonnee.java
@@ -1,2 +1,48 @@
-package gameLaby.laby;public class Coordonnee {
+package gameLaby.laby;
+
+public class Coordonnee {
+    private int x,y;
+
+    /**
+     * Contructeur de Coordonnée
+     * @param x coordonnée x
+     * @param y coordonnée y
+     */
+    Coordonnee(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+
+    /**
+     * getter du coordonnée x
+     * @return le coordonnée x
+     */
+    public int getX(){
+        return this.x;
+    }
+
+    /**
+     * getter du coordonnée y
+     * @return le coordonnée y
+     */
+    public int getY(){
+        return this.y;
+    }
+
+    /**
+     * setter de x
+     * @param x nouvel coordonnée a set
+     */
+    public void setX(int x){
+        this.x=x;
+    }
+
+    /**
+     * setter de y
+     * @param y nouvel coordonnée y
+     */
+    public void setY(int y){
+        this.y=y;
+    }
+
 }
