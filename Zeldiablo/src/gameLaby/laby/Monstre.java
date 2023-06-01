8295e674d152b029a51f661158359cd2fb6a01ed
nino.arcelin@gmail.com <101625357+Nin0A@users.noreply.github.com>
2023-06-01 10:44:19 +0200

    ajout de Monstre & Personnage

--- Zeldiablo/src/gameLaby/laby/Monstre.java
+++ Zeldiablo/src/gameLaby/laby/Monstre.java
@@ -4,12 +4,7 @@ package gameLaby.laby;
 /**
  * gere un personnage situe en x,y
  */
-public class Monstre implements Personnage {
-
-    /**
-     * position du personnage
-     */
-    int x, y;
+public class Monstre extends Personnage {
 
     /**
      * constructeur
@@ -18,39 +13,6 @@ public class Monstre implements Personnage {
      * @param dy position selon y
      */
     public Monstre(int dx, int dy) {
-        this.x = dx;
-        this.y = dy;
-    }
-
-    /**
-     * permet de savoir si le personnage est en x,y
-     *
-     * @param dx position testee
-     * @param dy position testee
-     * @return true si le personnage est bien en (dx,dy)
-     */
-    public boolean etrePresent(int dx, int dy) {
-
-        return (this.x == dx && this.y == dy);
-    }
-
-    // ############################################
-    // GETTER
-    // ############################################
-
-    /**
-     * @return position x du personnage
-     */
-    public int getX() {
-        // getter
-        return this.x;
-    }
-
-    /**
-     * @return position y du personnage
-     */
-    public int getY() {
-        //getter
-        return this.y;
+        super(dx,dy);
     }
 }
