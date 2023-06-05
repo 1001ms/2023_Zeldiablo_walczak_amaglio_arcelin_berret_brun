4c35dd00723a581b90720b0d35a512c04d2eafdc
nino.arcelin@gmail.com <101625357+Nin0A@users.noreply.github.com>
2023-06-05 10:02:06 +0200

    maj des tests

--- Zeldiablo/Test/LabyrintheTest.java
+++ Zeldiablo/Test/LabyrintheTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class LabyrintheTest {
     //!!!!!!!!!!!!!!! possible que les tests ne soit pas forcément juste car le déplacement des monstre est aléatoire
     // donc relancer si ce n'est pas juste !!!!!!!!!!!!!!!!!!
@@ -45,7 +47,7 @@ public class LabyrintheTest {
         int y = laby.pj.getY();
         int x = laby.pj.getX();
 
-        Assertions.assertEquals(yPrec, y, "Le déplacement vertical du personnage vers le haut s'est produite");
+        assertEquals(yPrec, y, "Le déplacement vertical du personnage vers le haut s'est produite");
         Assertions.assertNotEquals(xPrec, x, "Le déplacement horizontal du personnage vers le haut ne devrait pas avoir lieu");
     }
 
@@ -64,13 +66,9 @@ public class LabyrintheTest {
         int y = laby.pj.getY();
         int x = laby.pj.getX();
 
-        Assertions.assertEquals(yPrec, y, "Le mur bloque le personnage donc le y ne change pas");
-        Assertions.assertEquals(xPrec, x, "Le mur bloque le personnage donc le x ne change pas");
+        assertEquals(yPrec, y, "Le mur bloque le personnage donc le y ne change pas");
+        assertEquals(xPrec, x, "Le mur bloque le personnage donc le x ne change pas");
     }
-
-    /**
-     * test du déplacement du serpent
-     */
 
     /**
      * test du déplacement du serpent
@@ -87,7 +85,41 @@ public class LabyrintheTest {
         int x = serpent.getX();
         int y = serpent.getY();
 
-        Assertions.assertEquals(xPrec, x, "Le déplacement vertical du serpent vers le haut ne devrait pas avoir lieu");
-        Assertions.assertEquals(yPrec, y, "Le déplacement horizontal du serpent vers le haut s'est produit");
+        assertEquals(xPrec, x, "Le déplacement vertical du serpent vers le haut ne devrait pas avoir lieu");
+        assertEquals(yPrec, y, "Le déplacement horizontal du serpent vers le haut s'est produit");
         }
+
+    /**
+     * test deplacerPerso lorsque que le perso va dans la direction d'une caisse adjacente à une autre
+     * le perso ne devrait pas pouvoir déplacer deux caisses
+     *
+     * @throws Exception
+     */
+    @Test
+    public void test_deplacerPerso_2_CAISSES() throws Exception {
+        Labyrinthe laby = new Labyrinthe("Zeldiablo/labyTest/labyTest2.txt",0,new Echappatoire(),0);
+
+
+        laby.deplacerPerso(Labyrinthe.GAUCHE);
+
+        assertEquals(4, laby.pj.getX(), "Les coordonnées de personnage ne change pas");
+        assertEquals(2, laby.caisses.getElementByIndice(0).getX(), "Les coordonnées de la première caisse de change pas");
+        assertEquals(3, laby.caisses.getElementByIndice(1).getX(), "Les coordonnées de la seconde caisse ne change pas");
+    }
+
+    /**
+     * test deplacerPerso lorsqu'il y a une seul caisse, cette fois-ci le perso doit pouvoir
+     * déplacer la caisse
+     *
+     * @throws Exception
+     */
+    @Test
+    public void test_deplacerPerso_1_CAISSE() throws Exception {
+        Labyrinthe laby = new Labyrinthe("Zeldiablo/labyTest/labyTest3.txt",0,new Echappatoire(),0);
+
+        laby.deplacerPerso(Labyrinthe.HAUT);
+
+        assertEquals(3, laby.pj.getX(), "La getX doit renvoyer x-1");
+        assertEquals(2, laby.caisses.getElementByIndice(0).getX(), "La getX doit renvoyer x-1");
+    }
 }
